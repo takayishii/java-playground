c9d95dbdf7786ec401968d38b11ce9672ed5a04f
takayishii@abeam.com-at-046310048203 <takayishii@abeam.com>
2021-06-07 15:50:21 +0900

    stash2

--- src/App.java
+++ src/App.java
@@ -2,6 +2,8 @@ public class App {
   public static void main(String[] args) throws Exception {
     Long i = null;
     System.out.println(i);
+    System.out.println("Hello");
+    System.out.println("World");
   }
 
 }
